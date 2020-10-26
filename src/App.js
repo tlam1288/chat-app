4ac208f5bfafccead36224bbed0d1740567f3ce1
nhuanthi <tlam1288@gmail.com>
2020-10-26 15:37:09 -0700

    added InfoBar component and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join.js';
+import Chat from './components/Chat/Chat.js';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
